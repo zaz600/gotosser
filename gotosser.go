62dd04f50ce349eff11f8af9ab9632b1ff10276c
Dmitriy Denisov <zaz600@gmail.com>
2015-12-22 21:34:38 +1000

    часть 2

--- gotosser.go
+++ gotosser.go
@@ -1,28 +1,78 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
+
+	"github.com/hhkbp2/go-strftime"
 )
 
 const (
 	configFileName = "gotosser.yaml"
 )
 
-// scanLoop периодически просматривает конфиг
+var (
+	//контролируем число потоков processScanGroup
+	tokens = make(chan struct{}, 8)
+)
+
+func processItems(items []os.FileInfo, fullSrcDir string) {
+	for _, item := range items {
+		// обрабатываем только файлы. Не каталоги, символические ссылки и т.п.
+		if !item.Mode().IsRegular() {
+			continue
+		}
+		srcFile := item.Name()
+		fullSrcFilePath := filepath.Join(fullSrcDir, srcFile)
+		log.Println(fullSrcFilePath)
+	}
+}
+
+func processScanGroup(scangroup ScanGroup) {
+	//освобождаем токен после завершения процедуры
+	defer func() { <-tokens }()
+
+	for _, srcDir := range scangroup.SrcDirs {
+		//разворачиваем маску времени (%Y%m%d и т.п.), если есть в пути
+		fullSrcDir := strftime.Format(srcDir, time.Now())
+		abspath, err := filepath.Abs(fullSrcDir)
+		if err != nil {
+			log.Println("Ошибка вычисления абсолютного пути", srcDir, err)
+			continue
+		}
+		fullSrcDir = abspath
+		log.Println("Сканируем каталог", fullSrcDir)
+		//читаем содержимое каталога
+		items, err := ioutil.ReadDir(fullSrcDir)
+		if err != nil {
+			log.Println(err)
+			log.Printf("Обработка каталога завершена %s", fullSrcDir)
+			continue
+		}
+		//обрабатываем файлы
+		processItems(items, fullSrcDir)
+	}
+}
+
+// scanLoop просматривает конфиг и для каждого каталога-источника
+// запускает горутину processScanGroup
 func scanLoop(cfg *Config) {
-	//периодически просматриваем конфиг и помечаем каталоги из него для сканирования файлов внутри
 	for {
-		for _, scandir := range cfg.ScanGroups {
-			if scandir.Enabled != true {
+		for _, scangroup := range cfg.ScanGroups {
+			if scangroup.Enabled != true {
 				continue
 			}
-			for _, srcDir := range scandir.SrcDirs {
-				log.Println("Сканируем каталог", srcDir)
-			}
+			//захватываем токен.
+			//в этом месте будет пауза, если окажется,
+			//что число запущенных горутин processScanDir больше,
+			//чем вместимость tokens
+			tokens <- struct{}{}
+			go processScanGroup(scangroup)
 		}
 		time.Sleep(time.Duration(cfg.RescanInterval) * time.Second)
 	}

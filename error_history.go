50ced5bdc7e64185de747b28928f38757d29ab04
Dmitriy Denisov <zaz600@gmail.com>
2016-01-01 22:11:29 +1000

    Улучшено хранилище ошибок errorhistoryStorage
    Вместо хранения времени вместе с текстом ошибки, храним время и ошибку отдельно.
    Теперь в шаблоне есть доступ ко времени возникновения ошибки. Можно менять его формат, например.

--- error_history.go
+++ error_history.go
@@ -1,37 +1,41 @@
 ﻿package main
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
 
+//errorHistoryItem запись с текстом ошибки и временем её возникновения
+type errorHistoryItem struct {
+	Time time.Time
+	Msg  string
+}
+
 //хранит определенное количество сообщений об ошибках
 type errorHistoryStore struct {
 	sync.RWMutex
-	data     []string
+	data     []errorHistoryItem //[]string
 	MaxCount int
 }
 
 //Добавляет сообщение в историю ошибок
 func (e *errorHistoryStore) Add(s string) {
-	tm := time.Now().Format("2006-01-02 15:04:05")
 	e.Lock()
 	defer e.Unlock()
-	e.data = append(e.data, fmt.Sprintf("%s %s", tm, s))
+	e.data = append(e.data, errorHistoryItem{time.Now(), s})
 	if len(e.data) > e.MaxCount {
 		e.data = e.data[1:]
 	}
 }
 
 //Возвращает историю ошибок
-func (e *errorHistoryStore) Get() []string {
+func (e *errorHistoryStore) Get() []errorHistoryItem {
 	e.RLock()
 	e.RUnlock()
 	return e.data
 }
 
-//newErrorHistoryStore создает новый объект errorHistoryStore и возвращает его 
+//newErrorHistoryStore создает новый объект errorHistoryStore и возвращает его
 func newErrorHistoryStore(MaxCount int) *errorHistoryStore {
 	return &errorHistoryStore{MaxCount: MaxCount}
 }
